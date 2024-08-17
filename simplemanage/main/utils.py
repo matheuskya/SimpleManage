ca9663663f94ed68c3b0155f37917a4c103ac8d4
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2024-08-17 09:56:43 -0300

    atualizacao na imagem do menu

--- simplemanage/main/utils.py
+++ simplemanage/main/utils.py
@@ -8,29 +8,30 @@ def generate_menu_image():
     menu_items = Cardapio.objects.filter(state= True)
 
     #carregar imagem de background
-    background_path = os.path.join(settings.BASE_DIR, 'main/menu_image/background_pastel_green.jpg')
+    background_path = os.path.join(settings.BASE_DIR, 'main/menu_image/menu.png')
     img = Image.open(background_path)
 
     #criando uma imagem nova de fundo branco
     # img = Image.new("RGB", (800,600), color="white")
-    img = img.resize((800, 600))
+    # 470,600
+    img = img.resize((470, 700))
 
 
     d = ImageDraw.Draw(img)
 
     #pegando fonte ttf
-    font_path = os.path.join(settings.BASE_DIR, 'main/fonts/cookie-crisp-font/CookieCrisp-L36ly.ttf')
+    font_path = os.path.join(settings.BASE_DIR, "main/fonts/aovel-sans-rounded-font/AovelSansRounded-rdDL.ttf")
     font = ImageFont.truetype(font_path, 20)    
 
     #ajustanto o title para o centro
-    title_text = "Cardapio de hoje!                    - valor kg: 51,90"
+    title_text = f"Cardapio de hoje!                    - Valor/kg: 51,90"
     title_bbox = d.textbbox((0, 0), title_text, font=font)
     title_width = title_bbox[2] - title_bbox[0]
     title_x = (img.width - title_width) / 2
-    title_y = 10
+    title_y = 140
     d.text((title_x, title_y), title_text, fill=(0, 0, 0), font=font)
 
-    y_text = 50
+    y_text = 190
     for item in menu_items:
         text = f"{item.name}: {item.description}"
         d.text((10, y_text), text, fill=(0,0,0), font=font)
